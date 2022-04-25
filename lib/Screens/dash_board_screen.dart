581445a6fd98bd28168926d90973696c22cfd0b0
Boringlyf <Boringlyf@users.noreply.github.com>
2022-04-26 00:17:59 +0500

    Change GridView.builder to GridView.count

--- lib/Screens/dash_board_screen.dart
+++ lib/Screens/dash_board_screen.dart
@@ -54,32 +54,27 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
               margin: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
               height: 460,
               width: double.infinity,
-              child: GridView.builder(
-                  padding: const EdgeInsets.all(10.0),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 2.6 / 2,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 15,
-                  ),
-                  itemCount: _studentOptions.length,
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      child: Card(
-                        child: Column(children: [
+              child: GridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: 2.6 / 2,
+                shrinkWrap: true,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 15,
+                padding: const EdgeInsets.all(10.0),
+                children: [
+                  GestureDetector(
+                    child: Card(
+                      child: Column(
+                        children: [
                           Container(
                             padding: EdgeInsets.symmetric(vertical: 20),
-                            width: 100,
-                            height: 100,
-                            child:
-                                Image.network(_studentOptions[index].imgPath),
-                          ),
-                          Text(_studentOptions[index].name),
-                        ]),
+                          )
+                        ],
                       ),
-                      onTap: () {},
-                    );
-                  }),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
@@ -87,3 +82,32 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
     );
   }
 }
+
+
+
+// GridView.builder(
+//                   padding: const EdgeInsets.all(10.0),
+//                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//                     crossAxisCount: 2,
+//                     childAspectRatio: 2.6 / 2,
+//                     crossAxisSpacing: 10,
+//                     mainAxisSpacing: 15,
+//                   ),
+//                   itemCount: _studentOptions.length,
+//                   itemBuilder: (context, index) {
+//                     return GestureDetector(
+//                       child: Card(
+//                         child: Column(children: [
+//                           Container(
+//                             padding: EdgeInsets.symmetric(vertical: 20),
+//                             width: 100,
+//                             height: 100,
+//                             child:
+//                                 Image.network(_studentOptions[index].imgPath),
+//                           ),
+//                           Text(_studentOptions[index].name),
+//                         ]),
+//                       ),
+//                       onTap: () {},
+//                     );
+//                   })

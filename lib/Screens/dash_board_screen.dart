6cf23af09360d650eb9ce8e7ddce2ccc5df97403
Boringlyf <Boringlyf@users.noreply.github.com>
2022-04-30 17:14:52 +0500

    ADD Navigation to DashBoard

--- lib/Screens/dash_board_screen.dart
+++ lib/Screens/dash_board_screen.dart
@@ -1,4 +1,6 @@
 import 'package:edutech_app/Screens/student_quiz.dart';
+import 'package:edutech_app/Screens/subject_screens.dart';
+import 'package:edutech_app/Screens/view_announcementsscreen.dart';
 import 'package:edutech_app/providers/dash_board_providers.dart';
 import 'package:edutech_app/widgets/custom_page_header.dart';
 import 'package:flutter/material.dart';
@@ -16,32 +18,6 @@ class DashBoardScreen extends StatefulWidget {
 }
 
 class _DashBoardScreenState extends State<DashBoardScreen> {
-  List<DashboardModels> _studentOptions = [
-    DashboardModels(
-      imgPath: 'https://cdn-icons-png.flaticon.com/512/595/595990.png',
-      name: 'Announcements',
-    ),
-    DashboardModels(
-      imgPath: 'https://cdn-icons-png.flaticon.com/512/5678/5678058.png',
-      name: "Quiz",
-    ),
-    DashboardModels(
-      imgPath: 'https://cdn-icons-png.flaticon.com/512/4185/4185776.png',
-      name: 'Assignments',
-    ),
-    DashboardModels(
-      imgPath: 'https://cdn-icons-png.flaticon.com/512/1497/1497835.png',
-      name: 'Schedule',
-    ),
-    DashboardModels(
-      imgPath: 'https://cdn-icons-png.flaticon.com/512/1041/1041916.png',
-      name: 'Chat',
-    ),
-    DashboardModels(
-      imgPath: 'https://cdn-icons-png.flaticon.com/512/2641/2641409.png',
-      name: 'Results',
-    ),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,16 +38,163 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
                 mainAxisSpacing: 15,
                 padding: const EdgeInsets.all(10.0),
                 children: [
+                  //1
                   GestureDetector(
                     child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
                       child: Column(
                         children: [
                           Container(
                             padding: EdgeInsets.symmetric(vertical: 20),
-                          )
+                            width: 100,
+                            height: 100,
+                            child: Image.network(
+                                'https://cdn-icons-png.flaticon.com/512/595/595990.png'),
+                          ),
+                          Text('Announcements')
                         ],
                       ),
                     ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: ((context) => SubjectScreens()),
+                        ),
+                      );
+                    },
+                  ),
+                  //2
+                  GestureDetector(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                            width: 100,
+                            height: 100,
+                            child: Image.network(
+                                'https://cdn-icons-png.flaticon.com/512/5678/5678058.png'),
+                          ),
+                          Text('Quiz')
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context) => SubjectScreens()),
+                      );
+                    },
+                  ),
+                  //3
+                  GestureDetector(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                            width: 100,
+                            height: 100,
+                            child: Image.network(
+                                'https://cdn-icons-png.flaticon.com/512/4185/4185776.png'),
+                          ),
+                          Text('Assignments')
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: ((context) => SubjectScreens()),
+                        ),
+                      );
+                    },
+                  ),
+                  //4
+                  GestureDetector(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                            width: 100,
+                            height: 100,
+                            child: Image.network(
+                                'https://cdn-icons-png.flaticon.com/512/1497/1497835.png'),
+                          ),
+                          Text('Schedule')
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context) => SubjectScreens()),
+                      );
+                    },
+                  ),
+                  //5
+                  GestureDetector(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                            width: 100,
+                            height: 100,
+                            child: Image.network(
+                                'https://cdn-icons-png.flaticon.com/512/1041/1041916.png'),
+                          ),
+                          Text('Chat')
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context) => SubjectScreens()),
+                      );
+                    },
+                  ),
+                  //6
+                  GestureDetector(
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 20),
+                            width: 100,
+                            height: 100,
+                            child: Image.network(
+                                'https://cdn-icons-png.flaticon.com/512/2641/2641409.png'),
+                          ),
+                          Text('Results')
+                        ],
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                            builder: (context) => SubjectScreens()),
+                      );
+                    },
                   )
                 ],
               ),

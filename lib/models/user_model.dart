779f5e6b448e3fe959e0a7a29adae3ba78d40b10
Boringlyf <Boringlyf@users.noreply.github.com>
2022-11-12 20:01:08 +0500

    user provider and models added

---
+++ lib/models/user_model.dart
@@ -0,0 +1,10 @@
+// model for all users basic information
+import 'package:flutter/foundation.dart';
+
+class UserModel {
+  final String name;
+  final String email;
+  final String phone;
+
+  UserModel({required this.name, required this.email, required this.phone});
+}
---
+++ lib/providers/users_provider.dart
@@ -0,0 +1,10 @@
+import 'package:app/models/user_model.dart';
+import 'package:flutter/foundation.dart';
+
+class UsersProvider with ChangeNotifier {
+  List<UserModel> _users = [];
+
+  List<UserModel> get users {
+    return [..._users]; //returns a copy of the items in _users array
+  }
+}
